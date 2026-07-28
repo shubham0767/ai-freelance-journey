e7172938c891d8546a589fe3c423670026945a24
Shubham Shirke <shubhamxdff@gmail.com>
2026-07-29 00:55:55 +0530

    Day 8: Tuples, Dictionaries and Student Database

---
+++ day 8/tuple_example.py
@@ -0,0 +1,12 @@
+print("=" * 35)
+print("Tuple Example")
+print("=" * 35)
+
+colors = ("Red ","Blue","Green","Yellow")
+print("\n All Colors :")
+
+for color in colors:
+    print(color)
+
+print("\nFirst Colour :", colors[0]   )
+print("Last Color :",colors[-1])
