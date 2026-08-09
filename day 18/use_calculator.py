b03fea88427bf861394b41c6121319cf453fe3ef
Shubham Shirke <shubhamxdff@gmail.com>
2026-08-09 18:55:15 +0530

    Day 18: Modules Packages and pip

---
+++ day 18/use_calculator.py
@@ -0,0 +1,6 @@
+import my_calculator
+
+print("Addition:", my_calculator.add(10, 5))
+print("Subtraction:", my_calculator.subtract(10, 5))
+print("Multiplication:", my_calculator.multiply(10, 5))
+print("Division:", my_calculator.divide(10, 5))
