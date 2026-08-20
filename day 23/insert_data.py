07864583e5bb404ac88e9cf0fb193fea55221b01
shubham0767 <shubhamxdff@gmail.com>
2026-08-20 22:25:29 +0530

    Day 23: Create SQLite Tables with Foreign Key

---
+++ day 23/insert_data.py
@@ -0,0 +1,33 @@
+import sqlite3
+connection = sqlite3.connect("college.db")
+cursor=connection.cursor()
+
+courses=[
+    (1,"BCA"),
+    (2,"BBI")
+    (3,"BBA")
+]
+
+cursor.executemany("""
+INSERT OR IGNORE IINTO courses (course_id, course _name)
+VALUES(?,?)
+
+""",courses)
+
+students=[
+    (1, "Shubham", 21, 1, 85),
+    (2, "Rahul", 20, 1, 78),
+    (3, "Amit", 22, 2, 92),
+    (4, "Priya", 21, 3, 95)
+]
+
+cursor.executemany("""
+INSERT OR IGNORE INTO students
+(student_id,name,age,course_id,marks)
+VALUES(?,?,?,?)
+""",students)
+
+connection.commit()
+print("DATA inserted Successfully.")
+
+connection.close()
